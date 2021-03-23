d52cf3d21c5a6ca55fdaac7ca2fd734f78636b45
Gabriel Loureiro <loureirobiel@gmail.com>
2021-03-22 22:16:15 -0300

    update database factory

--- application/src/main/kotlin/com/loureiro/gamelist/config/DatabaseFactory.kt
+++ application/src/main/kotlin/com/loureiro/gamelist/config/DatabaseFactory.kt
@@ -1,13 +1,14 @@
 package com.loureiro.gamelist.config
 
 import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
 import com.mongodb.client.MongoDatabase
 
 object DatabaseFactory {
 
-    private val mongoHost = System.getenv().getOrDefault("MONGO_HOST", "127.0.0.1")
-    private val mongoPort = System.getenv().getOrDefault("MONGO_PORT", "27017").toInt()
     private val mongoDatabase = System.getenv().getOrDefault("MONGO_DATABASE", "Gamelist")
+    private val mongoHost = System.getenv().getOrDefault("MONGO_HOST", "127.0.0.1:27001")
+    private val uri: MongoClientURI = MongoClientURI(mongoHost)
 
-    val database: MongoDatabase = MongoClient(mongoHost, mongoPort).getDatabase(mongoDatabase)
+    val database: MongoDatabase = MongoClient(uri).getDatabase(mongoDatabase)
 }
